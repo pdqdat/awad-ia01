c0c069b5f3c251f7905afc6dfcc260137c831e60
Dat Phan <phanduongquocdat273@gmail.com>
2025-03-04 22:24:28 +0700

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/awp-ia01/",
+    base: "/awad-ia01/",
     plugins: [react()],
 });
